9dd1ff197b6038434ecc22252ae1d37c123ab3f5
hujiecai <hujc@thinkive.com>
2018-03-21 18:53:27 +0800

    添加cookie管理

---
+++ JcFramework/src/main/java/com/jc/framework/network/JcCookieJar.java
@@ -0,0 +1,15 @@
+package com.jc.framework.network;
+
+import okhttp3.CookieJar;
+
+/**
+ * @author Mr.Hu(Jc)
+ * @create 2018/3/11 22:10
+ * @describe
+ * @update
+ */
+
+public interface JcCookieJar extends CookieJar{
+
+    void clear();
+}
